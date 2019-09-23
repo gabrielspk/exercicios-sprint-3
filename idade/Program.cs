7a5bea4b1f3900c3d7af56615cc14dfb45bb91bb
gabrielspk <gabrielsilvaferreira007@gmail.com>
2019-09-23 17:20:45 -0300

    23/09

---
+++ idade/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace idade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int ano;
+        int idade = 0;
+
+    do{
+        Console.Write("Qual o ano de nascimento: ");
+        ano = int.Parse(Console.ReadLine());
+        if((ano > 2021) || (ano < 0)){
+            Console.WriteLine("Data inválida");
+        }
+      } while ((ano > 2019) || (ano < 0));
+               
+        idade = 2019 - ano;
+      
+        if(idade < 3) {
+            Console.WriteLine("Recém-nascido");
+        } else if ((idade >= 3) && (idade <= 11)){
+            Console.WriteLine("Criança");
+        }else if ((idade >= 12) && (idade <= 19)){
+            Console.WriteLine("Adolescente");
+        }else if((idade >= 20) && (idade <= 65)){
+            Console.WriteLine("Adulto");
+        } else if(idade > 65) {
+            Console.WriteLine("Idoso");
+        }
+   
+        }
+    }
+}
--- tabuada5/Program.cs
+++ tabuada5/Program.cs
@@ -6,9 +6,12 @@ namespace tabuada5
     {
         static void Main(string[] args)
         {
-            for (int i=1;i <=10; i++);
-
-            Console.WriteLine("");
+            for (int i=1;i <= 10; i++){
+                for(int j = 1; j <= 10; j++){
+                    Console.Write($"{i,-2} x {j,-2}= {i * j,-3}");
+            }
+                Console.WriteLine();
         }
     }
+}
 }
