6ea9962a77fff914747d6375abd4048ff1d89604
gabrielspk <gabrielsilvaferreira007@gmail.com>
2019-09-26 17:17:36 -0300

    29/09/19

--- matriz/Program.cs
+++ matriz/Program.cs
@@ -6,53 +6,26 @@ namespace matriz
     {
         static void Main(string[] args)
         {
-            int num0 = 0;
-            int num1 = 1;
-            int num2 = 2;
-            int num3 = 3;
-            int num4 = 4;
-            int num5 = 5;
-            
-            Console.Write("digite o primeiro número");
-            num0 = int.Parse(Console.ReadLine());
-            
-            int[] vetor = new int[6];
+            int[] vetor = new int [6];
+            int par = 0;
+            int impar = 0;
 
-            int[] vetor2 = {num0,num1,num2,num3,num4,num5};
+            for (int cont = 0; cont < 6; cont++){
+                Console.Write($"Digite o {cont +1 } número: ");
+               vetor[cont] = int.Parse(Console.ReadLine());
+            }
 
-            if (num0 % 2 == 0)
-            Console.WriteLine("É par");
-        else
-            Console.WriteLine("é impar");
-
-            if (num1 % 2 == 0)
-            Console.WriteLine("É par");
-        else
-            Console.WriteLine("é impar");
-
-            if (num2 % 2 == 0)
-             Console.WriteLine("É par");
-        else
-            Console.WriteLine("é impar");
-
-            if (num3 % 2 == 0)
-             Console.WriteLine("é par");
-        else
-            Console.WriteLine("é impar");
-
-            if (num4 % 2 == 0)
-            Console.WriteLine("é par");
-        else
-            Console.WriteLine("é impar");
-
-            if (num5 % 2 == 0)
-            Console.WriteLine("é par");
-        else
-            Console.WriteLine("é impar");
-
-            
-
-
+            foreach(int num in vetor){
+                if(num % 2 == 0){
+                    par++;
+                }else{
+                    impar++;
+                }
+            }
+             Console.WriteLine($"Você tem {par} números pares e {impar} números impares amigão");
         }
     }
 }
+
+           
+
--- matrizFor/Program.cs
+++ matrizFor/Program.cs
@@ -6,7 +6,30 @@ namespace matrizFor
     {
         static void Main(string[] args)
         {
-            
+            double[] temperaturas = new double[12];
+            double maior = 0;
+            double menor = 0;
+
+            for (int i = 0; i < 12; i ++)
+            {
+                Console.WriteLine($"Digite a temperatura do mês {i + 1}: ");
+                temperaturas[i] = double.Parse(Console.ReadLine());   
+            }
+
+            maior = temperaturas[0];
+            menor = temperaturas[0];
+
+            foreach(double temp in temperaturas)
+            {
+                if(temp > maior)
+                {
+                    maior = temp;
+                }else if(temp < menor)
+                {
+                    menor = temp;
+                }
+            }
+            Console.WriteLine($"A maior temperatura {maior} || a menor temperatura {menor}");
         }
     }
 }
