d34ae2e7aab93f57d8d2247e13152670d9eb0191
gabrielspk <gabrielsilvaferreira007@gmail.com>
2019-10-17 17:25:02 -0300

    17/10/19

--- EscolaDeRock/Models/Deposito.cs
+++ EscolaDeRock/Models/Deposito.cs
@@ -6,13 +6,13 @@ namespace EscolaDeRock.Models
     {
         public static Dictionary<int,InstrumentoMusical> Instrumentos = new Dictionary<int, InstrumentoMusical>()
         {
-            {1,new Baixo() },
-            {2,new Bateria() },
-            {3,new Contrabaixo() },
-            {4,new Guitarra() },
-            {5,new Teclado() },
-            {6,new Tambores() },
-            {7,new Violao() }
+            { 1, new Baixo() },
+            { 2, new Bateria() },
+            { 3, new Contrabaixo() },
+            { 4, new Guitarra() },
+            { 5, new Teclado() },
+            { 6, new Tambores() },
+            { 7, new Violao() }
         };
     }
 }
